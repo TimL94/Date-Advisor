8cf972ddaab3485e0c426d4d88c5eaac653797df
Stephen Bates <Stephen.Michael.Bates@outlook.com>
2024-02-02 09:56:42 -0500

    Added jquery function to fetch movie info and create DOM elements for them

---
+++ Assets/Scripts/movies.js
@@ -0,0 +1,33 @@
+var key = "5ddf64d5";
+var $movieList = $('#movie-list');
+
+function getMovieInfo(searchTerm){
+    fetch(`http://www.omdbapi.com/?apikey=${key}&s=${searchTerm}`)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        for(movie of data.Search){
+            fetch(`http://www.omdbapi.com/?apikey=${key}&i=${movie.imdbID}`)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                var $movieCard = $("<div>", {class: 'movie-card'})
+                //data.Title
+                $movieCard.append(`<h3 class = 'title'>${data.Title}</h3>`)
+                //data.Released
+                $movieCard.append(`<div class = 'release'>${data.Released}</div>`)
+                //data.imdbRating
+                $movieCard.append(`<div class = 'imdb-rating'>${data.imdbRating}</div>`)
+                //data.Poster
+                $movieCard.append(`<img class = 'poster' src = '${data.Poster}'></h3>`)
+                //
+                $movieList.append($movieCard);
+            })
+        }
+    })  
+}
+
+getMovieInfo("Batman");
